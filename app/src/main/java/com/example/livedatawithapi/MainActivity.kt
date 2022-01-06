48804bdf06f1ffc727515eeb6ab268055649339d
salim-maula <salim-maula>
2022-01-06 16:34:16 +0700

    Add Toast

--- app/src/main/java/com/example/livedatawithapi/MainActivity.kt
+++ app/src/main/java/com/example/livedatawithapi/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import com.bumptech.glide.Glide
 import com.example.livedatawithapi.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,6 +44,16 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel.isLoading.observe(this, {
             activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let { snackBarText->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
         })
 
         activityMainBinding.btnSend.setOnClickListener { view ->
